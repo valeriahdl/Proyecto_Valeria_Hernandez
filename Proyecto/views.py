0fa173dfeac647a67793054bee2158ebcc0f35f1
valeriahdl <valhdl7@gmail.com>
2023-07-15 20:53:29 -0600

    Ultimas modificaciones

--- Proyecto/views.py
+++ Proyecto/views.py
@@ -3,7 +3,7 @@ from django.template import Template, Context, loader
 from django.shortcuts import render
 
 def saludo(request):
-    return HttpResponse("<br><br><h1>Hola,</h1> Este es un test. Si estás aquí por favor escribe el servidor más: /SorteoTique/inicio en el buscador. <br>Si quieres acceder a la lista de ganadores pasados escribe el servidor más: /ExitoTique/inicio")
+    return HttpResponse("<br><br><h1>Hola,</h1> Este es un test. Si estás aquí por favor escribe el servidor más: /SorteoTique/inicio en el buscador. <br>Si quieres acceder a la lista de ganadores pasados escribe el servidor más: /TiqueExito/landing")
 
 def deSorteo(self, aleatorio):
     data = f"Lo que quiera escribir en negritas: <h1>{aleatorio}</h1>"
